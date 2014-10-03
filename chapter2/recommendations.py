f3c514898bd5f36a673833f65851f948b6011d06
Alexoner <onerhao@gmail.com>
2014-10-03 22:54:41 +0800

    ranking the critics,get top n similar user

--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -78,3 +78,17 @@ def sim_pearson(prefs, p1, p2):
     r = num / den
 
     return r
+
+# Ranking the Critics
+# Returns the best matches for person from preferences dictionary.
+# Number of results and similarity function are optional params.
+
+
+def topMatches(prefs, person, n=5, similarity=sim_pearson):
+    scores = [(similarity(prefs, person, other), other)
+              for other in prefs if other != person]
+
+    # sort the list so the highest scores appear at the top
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
