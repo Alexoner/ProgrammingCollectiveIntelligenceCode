ba59639b4eb80ea7409de1f9876bad4f641e3a0d
Alexoner <onerhao@gmail.com>
2014-10-07 19:43:29 +0800

    cluster:visualized hierarchy clusters

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -138,7 +138,86 @@ def printclust(clust, labels=None, n=0):
     if clust.right != None:
             printclust(clust.right, labels=labels, n=n + 1)
 
+# Drawing the dendromgram
+# interpret the clusters more clearly by viewing them as a dendrogram.
+
+# determine the overall height of the image
+try:
+    from PIL import Image, ImageDraw
+
+    def getheight(clust):
+        # Is this an endpoint? Then the height is just 1
+        if clust.left == None and clust.right == None:
+            return 1
+
+        # Otherwise the height is the same of the heights of each branch
+        return getheight(clust.left) + getheight(clust.right)
+
+    def getdepth(clust):
+        # The distance of an endpoint is 0.0
+        if clust.left == None and clust.right == None:
+            return 0
+
+        # The distance of a branch is the greater of its two sides
+        # plus its own distance
+        return max(getdepth(clust.left), getdepth(clust.right)) + clust.distance
+
+    # the drawdendrogram function creates a new image allowing 20 pixels
+    # in heights and a fixed width for each final cluster.The scaling
+    # factor is determined by dividing the fixed width by the total depth.
+    # scaling factor
+    def drawdendrogram(clust, labels, jpeg='clusters.jpg'):
+        # height and width
+        h = getheight(clust) * 20
+        w = 1200
+        depth = getdepth(clust)
+
+        # Width is fixed,so scale distances accordingly
+        scaling = float(w - 150) / depth
+
+        # Create a new image with a white background
+        img = Image.new('RGB', (w, h), (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        draw.line((0, h / 2, 10, h / 2), fill=(255, 0, 0))
+
+        # Draw the first node
+        drawnode(draw, clust, 10, (h / 2), scaling, labels)
+        img.save(jpeg, 'JPEG')
+
+    def drawnode(draw, clust, x, y, scaling, labels):
+        if clust.id < 0:
+            h1 = getheight(clust.left) * 20
+            h2 = getheight(clust.right) * 20
+            top = y - (h1 + h2) / 2
+            bottom = y + (h1 + h2) / 2
+            # Line length
+            ll = clust.distance * scaling
+            # Vertical line from this cluster to children
+            draw.line((x, top + h1 / 2, x, bottom - h2 / 2), fill=(255, 0, 0))
+
+            # Horizontal line to left item
+            draw.line(
+                (x, top + h1 / 2, x + ll, top + h1 / 2), fill=(255, 0, 0))
+
+            # Horizontal line to right item
+            draw.line(
+                (x, bottom - h2 / 2, x + ll, bottom - h2 / 2), fill=(255, 0, 0))
+
+            # Call the function to draw the left and right nodes
+            drawnode(draw, clust.left, x + ll, top + h1 / 2, scaling, labels)
+            drawnode(draw, clust.right, x + ll,
+                     bottom - h2 / 2, scaling, labels)
+        else:
+            # If this is an endpoint,draw the item label
+            draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
+
+except:
+    print "Python Imaging Library not found"
+
 if __name__ == "__main__":
     blogname, words, data = readfile("blogdata.txt")
     clust = hcluster(data)
     printclust(clust, labels=blogname)
+
+    drawdendrogram(clust, blogname, jpeg='blogcluster.jpg')
