5e83c0c3da5761a63e7e9b6f8e6fbaa0a1d90d54
Alexoner <onerhao@gmail.com>
2014-10-14 08:50:35 +0800

    Searching and Ranking:a simple crawler

--- chapter4/searchengine.py
+++ chapter4/searchengine.py
@@ -1,3 +1,4 @@
+# -*- encoding:utf-8 -*-
 '''
 Develop a way to collect documents:crawling
 
@@ -8,6 +9,14 @@ Return a ranked list of documents from a query.
 Build a neural network for ranking queries.
 
 '''
+
+import urllib2
+from BeautifulSoup import *
+from urlparse import urljoin
+
+
+# Create a list of words to ignore
+ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
 
 
 class crawler:
@@ -50,8 +59,40 @@ class crawler:
     # Starting with a list of pages,do a breadth first search to the given
     # depth,indexing pages as we go
     def crawl(self, pages, depth=2):
+        for i in range(depth):
+            newpages = set()
+            for page in pages:
+                try:
+                    c = urllib2.urlopen(page)
+                except:
+                    print "Could not open %s" % page
+                    continue
+                soup = BeautifulSoup(c.read())
+                self.addtoindex(page, soup)
+
+                links = soup('a')
+                for link in links:
+                    if ('href' in dict(link.attrs)):
+                        url = urljoin(page, link['href'])
+                        if url.find("'") != -1:
+                            continue
+                        url = url.split('#')[0]  # remove location portion
+                        if url[0:4] == 'http' and not self.isindexed(url):
+                            # print 'New url: %s' % url
+                            newpages.add(url)
+                        linkText = self.gettextonly(link)
+                        self.addlinkref(page, url, linkText)
+
+                self.dbcommit()
+
+            pages = newpages
         pass
 
     # Create the database tables
     def createindextables(self):
         pass
+
+if __name__ == "__main__":
+    pagelist = ['http://www.geeksforgeeks.org']
+    crawler = crawler('')
+    crawler.crawl(pagelist)
