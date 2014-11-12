bd80445dea27676fc50149168c1d62401bf9c19b
Alexoner <onerhao@gmail.com>
2014-11-12 23:07:21 +0800

    Searching and Ranking:derivative of tanh(x)

--- ProgrammingCollectiveIntelligence/4.Searching.and.Ranking/nn.py
+++ ProgrammingCollectiveIntelligence/4.Searching.and.Ranking/nn.py
@@ -10,6 +10,13 @@ from math import tanh
 from pysqlite2 import dbapi2 as sqlite
 
 
+def dtanh(y):
+    """
+    Derivative of y = tanh(x)
+    """
+    return 1.0 - y * y
+
+
 class searchnet:
 
     def __init__(self, dbname):
