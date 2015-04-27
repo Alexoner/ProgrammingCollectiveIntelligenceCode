c535c470581f8ac4e91aa41ace6368a6c06494d2
Alexoner <onerhao@gmail.com>
2015-04-28 00:35:02 +0800

    logistic regression,newton-raphson scheme

--- PRML/chapter04/classLogitBin.m
+++ PRML/chapter04/classLogitBin.m
@@ -1,48 +1,55 @@
 function [model, llh] = classLogitBin(X, t, lambda)
 % logistic regression for binary classification (Bernoulli likelihood)
-if any(unique(t) ~= [0,1])
+% Newton-Raphson iterative reweighted optimization scheme to solve the
+% optimization problem
+%if any(unique(t) ~= [0,1])
+if unique(t) ~= [0,1]
     error('t must be a 0/1 vector!');
 end
 if nargin < 3
     lambda = 1e-4;
 end
-[d,n] = size(X);
+[n,d] = size(X);
 dg = sub2ind([d,d],1:d,1:d);
-X = [X; ones(1,n)];
+%X = [X; ones(1,n)];
+X = [ones(n,1),X];
 d = d+1;
 
 tol = 1e-4;
+% maximum iteration number
 maxiter = 100;
+% log-likelihood function
 llh = -inf(1,maxiter);
 converged = false;
+% iteration pass index
 iter = 1;
 
 
-h = ones(1,n);
+h = ones(n,1);
 h(t==0) = -1;
 w = zeros(d,1);
-z = w'*X;
+z = X*w;
 while ~converged && iter < maxiter
     iter = iter + 1;
     y = sigmoid(z);
     
-    g = X*(y-t)'+lambda*w;
+    g = X'*(y-t)+lambda*w;
     r = y.*(1-y);
     R = spdiags(r(:),0,n,n);    
-    H = X*R*X';
+    H = X'*R*X;
     H(dg) = H(dg)+lambda;
     
     p = -H\g;
 
     wo = w;
     w = wo+p;
-    z = w'*X;   
+    z = X*w;   
     llh(iter) = -sum(log1pexp(-h.*z))-0.5*lambda*dot(w,w);
     converged = norm(p) < tol || abs(llh(iter)-llh(iter-1)) < tol;
     while ~converged && llh(iter) < llh(iter-1)
         p = 0.5*p;
         w = wo+p;
-        z = w'*X;    
+        z = X*w;    
         llh(iter) = -sum(log1pexp(-h.*z))-0.5*lambda*dot(w,w);
         converged = norm(p) < tol || abs(llh(iter)-llh(iter-1)) < tol;
     end
