65210f82b12864d125b4ae323645cc1c7f60ac27
Alexoner <onerhao@gmail.com>
2015-04-28 23:33:27 +0800

    bayesian linear regression evidence approximation

--- PRML/chapter03/regressEbFp.m
+++ PRML/chapter03/regressEbFp.m
@@ -22,22 +22,30 @@ tol = 1e-4;
 maxiter = 100;
 llh = -inf(1,maxiter+1);
 for iter = 2:maxiter
+    % Hessian matrix (3.81)
     A = beta*C;
     A(dg) = A(dg)+alpha;
+    % U'*U = A
     U = chol(A);
     V = U\I;
 
+    % (3.84)
     w = beta*(V*(V'*Xt));
+    % L2 regularization
     w2 = dot(w,w);
+    % sum of squares error
     err = sum((t-w'*X).^2);
-    
+    % log|A|
     logdetA = 2*sum(log(diag(U)));    
+    % evidence approximation: log of the marginal likelihood (3.86)
     llh(iter) = 0.5*(d*log(alpha)+n*log(beta)-alpha*w2-beta*err-logdetA-n*log(2*pi)); 
     if llh(iter)-llh(iter-1) < tol*abs(llh(iter-1)); break; end
     
     trS = dot(V(:),V(:));
     gamma = d-alpha*trS;
+    % (3.92)
     alpha = gamma/w2;
+    % (3.95)
     beta = (n-gamma)/err;
 end
 b = tbar-dot(w,xbar);
