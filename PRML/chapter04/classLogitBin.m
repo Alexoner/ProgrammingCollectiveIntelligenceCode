62231c6554e13c0d2091bb24d3ecaf481c05d779
Alexoner <onerhao@gmail.com>
2015-09-11 16:37:00 +0800

    logistic regression,binary

--- PRML/chapter04/classLogitBin.m
+++ PRML/chapter04/classLogitBin.m
@@ -2,23 +2,33 @@ function [model, llh] = classLogitBin(X, t, lambda)
 % logistic regression for binary classification (Bernoulli likelihood)
 % Newton-Raphson iterative reweighted optimization scheme to solve the
 % optimization problem
-%if any(unique(t) ~= [0,1])
-if unique(t) ~= [0,1]
+% @X: input n*d design matrix
+% @t: n*1 response
+% @lambda: regularization coefficient
+% @model:
+% @llh: likelihood function
+
+% any(): return true if any element of the vector is non-zero
+if any(unique(t) ~= [0;1])
+    disp(unique(t));
     error('t must be a 0/1 vector!');
 end
 if nargin < 3
     lambda = 1e-4;
 end
-[n,d] = size(X);
+[n,d] = size(X)
+% sub2ind: subscripts to linear index
 dg = sub2ind([d,d],1:d,1:d);
 %X = [X; ones(1,n)];
 X = [ones(n,1),X];
 d = d+1;
 
+% difference tolerance
 tol = 1e-4;
 % maximum iteration number
 maxiter = 100;
 % log-likelihood function
+% inf(N,M):scalar,matrix whose elements are infinity
 llh = -inf(1,maxiter);
 converged = false;
 % iteration pass index
@@ -28,29 +38,32 @@ iter = 1;
 h = ones(n,1);
 h(t==0) = -1;
 w = zeros(d,1);
-z = X*w;
+a = X*w;
 while ~converged && iter < maxiter
     iter = iter + 1;
-    y = sigmoid(z);
+    y = sigmoid(a);
     
-    g = X'*(y-t)+lambda*w;
+    gradient = X'*(y-t)+lambda*w;
     r = y.*(1-y);
+    % equation (4.98)
     R = spdiags(r(:),0,n,n);    
-    H = X'*R*X;
-    H(dg) = H(dg)+lambda;
+    Hessian = X'*R*X;
+    Hessian(dg) = Hessian(dg)+lambda;
     
-    p = -H\g;
+    % equation (4.92):Newton-Raphson iterative
+    p = -Hessian\gradient;
 
     wo = w;
     w = wo+p;
-    z = X*w;   
-    llh(iter) = -sum(log1pexp(-h.*z))-0.5*lambda*dot(w,w);
-    converged = norm(p) < tol || abs(llh(iter)-llh(iter-1)) < tol;
+    a = X*w;   
+    llh(iter) = -sum(log1pexp(-h.*a))-0.5*lambda*dot(w,w);
+    converged = norm(p) < tol || abs(llh(iter)-llh(iter-1)) < tol
     while ~converged && llh(iter) < llh(iter-1)
+        disp(p);
         p = 0.5*p;
         w = wo+p;
-        z = X*w;    
-        llh(iter) = -sum(log1pexp(-h.*z))-0.5*lambda*dot(w,w);
+        a = X*w;    
+        llh(iter) = -sum(log1pexp(-h.*a))-0.5*lambda*dot(w,w);
         converged = norm(p) < tol || abs(llh(iter)-llh(iter-1)) < tol;
     end
 end
--- PRML/chapter04/demo.m
+++ PRML/chapter04/demo.m
@@ -8,18 +8,25 @@ n = 1000;
 X = X';
 t = t';
 
-[x1,x2] = meshgrid(linspace(min(X(1,:)),max(X(1,:)),n), linspace(min(X(2,:)),max(X(2,:)),n));
+% linspace (BASE, LIMIT, N):
+% Return a row vector with N linearly spaced elements between BASE and LIMIT.
+% meshgrid():
+%   Given vectors of X and Y coordinates, return matrices XX and YY corresponding to a full 2-D grid
+%   XX: copies of rows from X, as one dimension's coordinate;
+%   YY: copies of columns from Y,another dimension's coordinate
+[x1,x2] = meshgrid(linspace(min(X(:,1)),max(X(:,1)),n), linspace(min(X(:,2)),max(X(:,2)),n));
 [model, llh] = classLogitBin(X,t-1);
 plot(llh);
-pause;
 figure;
 spread(X',t');
 
 w = model.w;
-y = w(1)*x1+w(2)*x2+w(3);
+%y = w(1)*x1+w(2)*x2+w(3);
+y = w(2)*x1+w(3)*x2+w(1);
 
 hold on;
 pause;
+% plot 1 contour of y,given coordinates composed of x1 and x2
 contour(x1,x2,y,1);
 hold off;
 %%
